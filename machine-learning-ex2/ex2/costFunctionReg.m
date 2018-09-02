857b37c663dcc1cbcbb089c691388b2ced0335ac
Yahu Gao <gao_yahu@163.com>
2018-09-02 09:50:41 +0800

    exercise 2 submit without optional
    Signed-off-by: Yahu Gao <gao_yahu@163.com>

--- machine-learning-ex2/ex2/costFunctionReg.m
+++ machine-learning-ex2/ex2/costFunctionReg.m
@@ -17,10 +17,15 @@ grad = zeros(size(theta));
 %               Compute the partial derivatives and set grad to the partial
 %               derivatives of the cost w.r.t. each parameter in theta
 
+h_theta = sigmoid(X * theta);
+J = sum(-y .* log(h_theta) - (1 - y) .* log(1 - h_theta))/m + lambda * sum(theta([2:end], 1) .^ 2) / 2 / m;
 
+delta = h_theta - y;
 
-
-
+% theta[2-end] penalized
+grad = (X' * delta)/m + lambda / m * theta;
+% theta0 by x0 = 1 and shouldn't be penalized
+grad(1, :) = sum(h_theta - y)/m;
 
 % =============================================================
 
