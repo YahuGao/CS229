cdd6cc6117aeadb55e7a27b9f5f4b16a595d4bc2
Yahu Gao <gao_yahu@163.com>
2018-09-11 23:14:47 +0800

    ex4: [Done]add regularization for gradient
    should not be regularizing the first column of theta which is used
    for the bias item.
    
    Signed-off-by: Yahu Gao <gao_yahu@163.com>

--- machine-learning-ex4/ex4/nnCostFunction.m
+++ machine-learning-ex4/ex4/nnCostFunction.m
@@ -107,8 +107,13 @@ for t = 1:m
     Theta1_grad = Theta1_grad + delta2 * (a1_t)';           % 25x1 * 1x401 ==> 25x401
 end
 
-Theta1_grad = Theta1_grad / m;
-Theta2_grad = Theta2_grad / m;
+% Unregularization gradient for theta
+% Theta1_grad = Theta1_grad / m;
+% Theta2_grad = Theta2_grad / m;
+
+% Add regularization gradient
+Theta1_grad = [Theta1_grad(:, 1) Theta1_grad(:, [2:end]) + lambda * Theta1(:, [2:end])] / m;   % [25x1 25x400] ==> 25x401
+Theta2_grad = [Theta2_grad(:, 1) Theta2_grad(:, [2:end]) + lambda * Theta2(:, [2:end])] / m;   % [10x1 10x25] ==> 10x26
 
 
 
