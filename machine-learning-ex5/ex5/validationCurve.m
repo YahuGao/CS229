976df966368f4cd889b77ef1aaf5f795115f5e68
Yahu Gao <gao_yahu@163.com>
2018-09-17 23:21:14 +0800

    ex5: get the best lambda value
    Signed-off-by: Yahu Gao <gao_yahu@163.com>

--- machine-learning-ex5/ex5/validationCurve.m
+++ machine-learning-ex5/ex5/validationCurve.m
@@ -38,9 +38,13 @@ error_val = zeros(length(lambda_vec), 1);
 %       end
 %
 %
+for i = 1:length(lambda_vec)
+    lambda = lambda_vec(i);
+    theta = trainLinearReg(X, y, lambda);
+    error_train(i) = linearRegCostFunction(X, y, theta, lambda)(1);
+    error_val(i) = linearRegCostFunction(Xval, yval, theta, lambda)(1);
 
-
-
+end
 
 
 
